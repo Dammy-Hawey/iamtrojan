a9311948a1d0ac51590b2703f19ca100866b2170
Damola David Awowole <dammyhaweydamola@gmail.com>
2025-07-29 15:55:37 -0500

    Update certificate.js
    updated

--- models/certificate.js
+++ models/certificate.js
@@ -1,12 +1,25 @@
+// const mongoose = require("mongoose");
+
+// const certSchema = new mongoose.Schema({
+//   name: String,
+//   certId: String,
+//   score: Number,
+//   level: String,
+//   date: String,
+//   createdAt: { type: Date, default: Date.now }
+// });
+
+// module.exports = mongoose.model("Certificate", certSchema);
+
 const mongoose = require("mongoose");
 
-const certSchema = new mongoose.Schema({
-  name: String,
-  certId: String,
-  score: Number,
-  level: String,
-  date: String,
+const certificateSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  score: { type: Number, required: true },
+  certId: { type: String, required: true, unique: true },
+  date: { type: String, required: true },
+  level: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Certificate", certSchema);
+module.exports = mongoose.model("Certificate", certificateSchema);
